a2c1653de295faefd75e40685ea3e21d943057fc
tomahawk-jupiter <brianbrainrogers@gmail.com>
2021-12-09 19:57:45 +0000

    First Version

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather App
+
+[OpenWeather Docs](https://openweathermap.org/current)
+
+
+#### Formula
+
+To convert temperatures in degrees Celsius to Fahrenheit **multiply by 1.8 (or 9/5) and add 32**.
+
+#### Degrees Symbol
+
+Press and hold the ALT key and type 0 1 7 6 on the numeric keypad of your keyboard. Make sure the NumLock is on.
---
+++ index.js
@@ -0,0 +1,59 @@
+const options = { mode: 'cors' };
+const apiKey = null;
+
+// ASYNC & AWAIT
+async function getWeather(cityName) {
+  if (apiKey) {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+    const response = await fetch(url, options);
+    const data = await response.json();
+    const weather = data.weather[0].main;
+    const city = data.name;
+    const country = data.sys.country;
+    const temp = data.main.temp;
+    const description = data.weather[0].description;
+    return { weather, city, country, temp, description };
+  } else {
+    alert('API key removed from project.')
+  }
+}
+
+const input = document.querySelector('.input');
+const searchBtn = document.querySelector('.search-btn');
+const mainWeather = document.querySelector('.main-weather');
+const cityCountry = document.querySelector('.location');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const unitsBtn = document.querySelector('.unit');
+
+const displayData = async (dataObj) => {
+  const d = await dataObj;
+  mainWeather.innerText = d.weather;
+  cityCountry.innerText = `${d.city}, ${d.country}`;
+  temperature.innerText = `${Math.round(d.temp)}°C`;
+  description.innerText = d.description;
+}
+
+const handleSearch = () => {
+    const dataObj = getWeather(input.value);
+    displayData(dataObj);
+}
+
+const handleUnits = () => {
+  const temp = temperature.innerText;
+  const oldValue = temp.slice(0, -2);
+  const unit = temp.slice(-2);
+  let newUnit;
+  let newValue;
+  if (unit === '°C') {
+    newUnit = '°F';
+    newValue = (oldValue * 1.8) + 32;
+  } else if (unit === '°F') {
+    newUnit = '°C';
+    newValue = (oldValue - 32) * (5/9);
+  }
+  temperature.innerText = Math.round(newValue) + newUnit;
+}
+
+searchBtn.addEventListener('click', handleSearch);
+unitsBtn.addEventListener('click', handleUnits);
