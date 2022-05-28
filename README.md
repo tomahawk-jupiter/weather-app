8dd159b3f8b95c427c031501fbf80cf2e49f0bd9
jupiter <brianbrainrogers@gmail.com>
2022-05-28 16:55:03 +0100

    Quick revision

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # Weather App
 
-[OpenWeather Docs](https://openweathermap.org/current)
+A simple app that gets the current weather for a specified city.
 
+The temperature can be displayed in Fahrenheit or Celsius by toggling a button.
 
-#### Formula
+OpenWeather [using the API](https://openweathermap.org/current).
+
+## API Key
+
+I removed the api key for security. Even though I used a free api key I thought it best not to expose it.
+
+You can sign up and get a free api key [here](https://openweathermap.org/price).
+
+Copy your api key into the `index.js` file assigned to the variable `apiKey`.
+
+## Formula
 
 To convert temperatures in degrees Celsius to Fahrenheit **multiply by 1.8 (or 9/5) and add 32**.
 
-#### Degrees Symbol
+## Degrees Symbol
 
-Press and hold the ALT key and type 0 1 7 6 on the numeric keypad of your keyboard. Make sure the NumLock is on.
+Press and hold the ALT key and type 0 1 7 6 on the numeric keypad of your keyboard. Make sure the NumLock is on. **Note**: this worked when I tried it before (I was using Windows) but not now (using Ubuntu).
+
+## Async & Await
+
+Theres an example of using async and await in the `index.js` file.
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const options = { mode: 'cors' };
+const options = { mode: "cors" };
 const apiKey = null;
 
 // ASYNC & AWAIT
@@ -14,17 +14,17 @@ async function getWeather(cityName) {
     const description = data.weather[0].description;
     return { weather, city, country, temp, description };
   } else {
-    alert('API key removed from project.')
+    alert("API key removed from project for security.");
   }
 }
 
-const input = document.querySelector('.input');
-const searchBtn = document.querySelector('.search-btn');
-const mainWeather = document.querySelector('.main-weather');
-const cityCountry = document.querySelector('.location');
-const temperature = document.querySelector('.temperature');
-const description = document.querySelector('.description');
-const unitsBtn = document.querySelector('.unit');
+const input = document.querySelector(".input");
+const searchBtn = document.querySelector(".search-btn");
+const mainWeather = document.querySelector(".main-weather");
+const cityCountry = document.querySelector(".location");
+const temperature = document.querySelector(".temperature");
+const description = document.querySelector(".description");
+const unitsBtn = document.querySelector(".unit");
 
 const displayData = async (dataObj) => {
   const d = await dataObj;
@@ -32,28 +32,32 @@ const displayData = async (dataObj) => {
   cityCountry.innerText = `${d.city}, ${d.country}`;
   temperature.innerText = `${Math.round(d.temp)}°C`;
   description.innerText = d.description;
-}
+};
 
 const handleSearch = () => {
-    const dataObj = getWeather(input.value);
-    displayData(dataObj);
-}
+  const dataObj = getWeather(input.value);
+  displayData(dataObj);
+};
 
 const handleUnits = () => {
   const temp = temperature.innerText;
   const oldValue = temp.slice(0, -2);
   const unit = temp.slice(-2);
   let newUnit;
+  let prevUnit;
   let newValue;
-  if (unit === '°C') {
-    newUnit = '°F';
-    newValue = (oldValue * 1.8) + 32;
-  } else if (unit === '°F') {
-    newUnit = '°C';
-    newValue = (oldValue - 32) * (5/9);
+  if (unit === "°C") {
+    newUnit = "°F";
+    prevUnit = "°C";
+    newValue = oldValue * 1.8 + 32;
+  } else if (unit === "°F") {
+    newUnit = "°C";
+    prevUnit = "°F";
+    newValue = (oldValue - 32) * (5 / 9);
   }
   temperature.innerText = Math.round(newValue) + newUnit;
-}
+  unitsBtn.innerText = prevUnit;
+};
 
-searchBtn.addEventListener('click', handleSearch);
-unitsBtn.addEventListener('click', handleUnits);
+searchBtn.addEventListener("click", handleSearch);
+unitsBtn.addEventListener("click", handleUnits);
